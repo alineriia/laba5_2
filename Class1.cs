9e7726eb888c3e436b96301118b9743de6f581e5
Аліна Шелест <Al.Shelest@ssu.edu.ua>
2020-04-01 14:44:57 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Book[] A = new Book[n];
+            A[0] = new Book("Jerome", "Salinger", "The Catcher in the Rye", "QE132", 1951, 234, "Novel");
+            A[1] = new Book("Elizabeth", "Gilbert", "Eat Pray Love: One Woman's Search for Everything Across Italy, India, and Indonesia", "DF324", 2006, 352, "Memoir");
+            A[2] = new Book("Agatha ", "Christie", "And Then There Were None", "JN543", 1939, 272, "Mystery novel");
+            A[3] = new Book("Miguel ", "de Cervantes", "Don Quixote", "VD976", 1605, 863, "Novel");
+            A[4] = new Book("Francis Scott ", "Fitzgerald", "The Great Gatsby", "OX593", 1925, 218, "Novel");
+
+            Console.WriteLine("             L I B R A R Y");
+            for (int i = 0; i < A.Length; i++)
+                Console.WriteLine(A[i]);
+            Console.WriteLine("Search by code:");
+             string z_code;
+             Console.WriteLine("Enter code for search:");
+             z_code = Console.ReadLine();
+             Book searchBook = new Book();
+             searchBook = searchBook.SearchByCode(A, ref z_code);
+             if (searchBook != null)
+               {
+                 Console.WriteLine(searchBook);
+               }
+             else Console.WriteLine("Book with this code does not exist");
+            Console.WriteLine("Compare of 2 books:");
+            Console.Write("Enter what you want to compare by \n***author or genre***. \nEnter categorie : ");
+            string choice = Console.ReadLine();
+            Console.WriteLine("Enter two № of books (1,2,3,4,5) to compare:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            switch (choice) 
+            {
+                case "author":
+                        A[a - 1].CompareByAuthor(A[b - 1]);
+                        break;
+                    case "genre":
+                        A[a - 1].CompareGenre(A[b - 1]);
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*Описати клас «Книга в бібліотеці».
+Клас повинен включати в себе наступні поля (властивості):
+• Прізвище ім’я по-батькові автора
+• Назва книги
+• Код
+• Рік видання
+• Кількість сторінок
+• Жанр
+o Визначити конструктор без параметрів, конструктори з параметрами.
+o Продумати, які властивості повинні бути доступні тільки для перегляду, а які - і для
+зміни/установки значення.
+o Визначити методи для порівняння двох книг за вказаним параметром.
+o Реалізувати пошук книги по коду.
+o Обов’язково реалізувати перевірку та обробку всіх нештатних ситуацій: книги з таким номер
+не існує, книг взагалі немає в списку, в поле коде встановлюються знаки пунктуації та інше.
+o Всі класи описати в окремих файлах.
+o Перевантажити стандартний метод toString();, для виводу інформації про книгу на екран.
+o Книг повинно бути не менше п’яти.
+*/
+
+namespace Lab5_2
+{
+    class Book
+    {
+        string AuthorName;
+        string SurName;
+        string Name;
+        string Code;
+        int Year;
+        int Count;
+        string Genre;
+        public Book()
+        {
+
+        }
+        public Book(string a,string s,string n,string c, int y, int cc, string g)
+        {
+            AuthorName = a;
+            SurName = s;
+            Name = n;
+            Code = c;
+            Year = y;
+            Count = cc;
+            Genre = g;
+        }
+        public int CompareGenre(Book A)
+        {
+            if (Genre.CompareTo(A.Genre) == 0)
+            {
+                Console.WriteLine("Books '{0}' and '{1}' have same genre;", Name, A.Name);
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine("Books '{0}' and '{1}' have different genre;", Name, A.Name);
+                return 0;
+            }
+        }
+
+        public Book SearchByCode(Book[] A, ref string z_code)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                if ((String.Equals(A[i].Code, z_code)))
+                    return A[i];
+            }
+            return null;
+        }
+        public override string ToString()
+        {
+            string str = String.Empty;
+            str += ("Name of the book      -    " + Name + ";\n");
+            str += ("Author        -    " + AuthorName + " " + SurName + ";\n");
+            str += ("Code      -    " + Code + ";\n");
+            str += ("Year      -   " + Year + ";\n");
+            str += ("Count     -    " + Count + ";\n");
+            str += ("Genre     -   " + Genre + ";\n");
+            return str;
+        }
+
+
+        public int CompareByAuthor(Book A)
+        {
+            if (AuthorName.CompareTo(A.AuthorName) == 0 && SurName.CompareTo(A.SurName) == 0)
+            {
+                Console.WriteLine("Books '{0}' and '{1}' have same author - {2} {3};", Name, A.Name, A.AuthorName, A.SurName);
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine("Books '{0}' and '{1}' have different authors - {2} {3};", Name, A.Name, A.AuthorName, A.SurName);
+                return 0;
+            }
+        }
+
+
+    }
+}
